50cd6a417f1874c14343de588d33922e13985051
sy1445307837 <sy1445307837@163.com>
2014-08-25 16:53:34 +0800

    2014-08-25
    实现多人在同一平台聊天

---
+++ chat/hytc.chat-online/hytc.chat-online/FrmMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace hytc.chat_online
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(301, 227);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(12, 12);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(364, 21);
+            this.txtIp.TabIndex = 1;
+            this.txtIp.Text = "255.255.255.255";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 186);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(364, 21);
+            this.txtMsg.TabIndex = 2;
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(12, 39);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistory.Size = new System.Drawing.Size(364, 126);
+            this.txtHistory.TabIndex = 3;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 262);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.btnSend);
+            this.Name = "FrmMain";
+            this.Text = "聊天";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtHistory;
+    }
+}
+
---
+++ chat/hytc.chat-online/hytc.chat-online/FrmMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace hytc.chat_online
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string ip=this.txtIp.Text;
+            UdpClient uc = new UdpClient();
+            string msg ="PUBLIC|"+ this.txtMsg.Text+"|shiyue";
+            byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep=new IPEndPoint(IPAddress.Parse(ip),9527);
+            uc.Send(bmsg,bmsg.Length,ipep);
+        }
+
+        private void listen() {
+            UdpClient uc = new UdpClient(9527);
+            while (true)
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+                byte[] bmsg = uc.Receive(ref ipep);
+                string msg = Encoding.Default.GetString(bmsg);
+                this.txtHistory.Text += msg + "\r\n";//
+            }
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            FrmMain.CheckForIllegalCrossThreadCalls = false;
+            Thread th = new Thread(new ThreadStart(listen));
+            th.IsBackground = true;
+            th.Start();
+        }
+    }
+}
